c07c809c4fad4467e369dcbcc27768922816e9b6
ljy <makelotus0209@gmail.com>
2025-07-23 14:09:25 +0900

    자바테스트 추가

---
+++ src/ch01_variable_operator/Add.java
@@ -0,0 +1,16 @@
+package ch01_variable_operator;
+
+public class Add {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        int c = a * b;
+
+        String message;
+
+        message = "a 곱하기 b는(은) "+ c + "입니다.";
+
+        System.out.println(message);
+
+    }
+}
--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -1,4 +1,22 @@
 package ch01_variable_operator;
 
+/*
+ * 작성일 : 이제용
+ * 작성일 : 7월 23일
+ *
+ *
+ * */
 public class MyPrint {
+    public static void main(String[] args) {
+        System.out.println("홍");
+        System.out.println();
+        System.out.println("길");
+        System.out.println('동');
+        System.out.println("대한 민국 화이팅");
+        System.out.println("\"예체능\" 프로그램 메인 MC \'강호동\'");
+
+        System.out.println("이름\t국어\t영어\t수학");
+        System.out.println("김철수\t80\t70\t60");
+        System.out.println("박영희\t40\t50\t60");
+    }
 }
