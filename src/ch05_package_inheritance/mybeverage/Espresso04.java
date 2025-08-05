d35eb8b51e8c568f807a15aa0135d63d904aff98
ljy <makelotus0209@gmail.com>
2025-08-05 10:41:49 +0900

    수정

--- src/ch05_package_inheritance/mybeverage/Espresso04.java
+++ src/ch05_package_inheritance/mybeverage/Espresso04.java
@@ -1,5 +1,14 @@
 package ch05_package_inheritance.mybeverage;
 
-public class Espresso04 {
+public class Espresso04 extends Beverage04{
     private int shotCount;
+
+    public Espresso04(String name, double price, int shotCount) {
+        super(name, price);
+        this.shotCount = shotCount;
+    }
+    public void drinkEspresso() {
+        String message = "맛이 진하고 강렬한 %s을(를) 마십니다.%n";
+        System.out.printf(message, super.getName());
+    }
 }
