3eb0fd9cc2a56cc2e9f224ccc440c92da08612fb
ljy <makelotus0209@gmail.com>
2025-08-08 13:07:22 +0900

    Mymap 추가

---
+++ src/ch07_collection/MyMap.java
@@ -0,0 +1,14 @@
+package ch07_collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyMap {
+    public static void main(String[] args) {
+        Map<String, Integer> teamMap = new HashMap<String, Integer>();
+
+        
+
+        System.out.println("요소 크기 : " + teamMap);
+    }
+}
